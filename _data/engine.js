420dfbeb6b9b39460a7de7944c2e6c85c60e57ac
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-11-18 20:49:05 +0100

    more layouting

--- _data/engine.js
+++ _data/engine.js
@@ -2,7 +2,7 @@ const {QueryEngineComunica} = require('graphql-ld-comunica/index');
 const comunicaConfig = {
   sources: [
     { type: "hypermedia", value: "https://data.dancehallbattle.org/data" },
-    { type: "hypermedia", value: "http://localhost:3000/all" },
+    { type: "hypermedia", value: "https://data.dev.dancehallbattle.org/rankings" },
   ],
 };
 
