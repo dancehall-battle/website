7d1f617405e8523fdca76aa504077521848bde96
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-12-13 08:29:33 +0100

    fix #80

--- serve.sh
+++ serve.sh
@@ -5,21 +5,29 @@ trap ctrl_c INT
 
 function ctrl_c() {
   rm -rf _cache
+  rm -rf _data
+  rm -f .eleventyignore
   exit 0
 }
 
 # Remove old folders
 rm -rf _cache # Should already be removed, but just in case
 rm -rf _site
+rm -rf _data
 
 # Create needed folders
 mkdir _cache
+mkdir _data
+cp -R _data-core/* _data
+cp -R _data-dancer/* _data
+cp -R _data-ranking/* _data
+cp .eleventyignore.serve .eleventyignore
 
-#envVars="ELEVENTY_SERVE=true SKIP=upcoming.json,battles.json,countries.json,dancer-list.json,dancers.json,events.json,country-to-battles-per-year.json,country-to-events.json"
-envVars="ELEVENTY_SERVE=true"
+envVars="ELEVENTY_SERVE=true SKIP=upcoming.json,battles.json,countries.json,dancer-list.json,dancers.json,events.json,country-to-battles-per-year.json,country-to-events.json"
+#envVars="ELEVENTY_SERVE=true"
 
 if [ "$1" == "-v" ]; then
     envVars="$envVars DEBUG=Eleventy*"
 fi
 
-eval $envVars node ./node_modules/@11ty/eleventy/cmd.js --serve --port 8080
+eval $envVars node ./node_modules/@11ty/eleventy/cmd.js --serve --port 8080 --input=. --output=_site
