79c32a2386399bee78a3202f4559b98a17deb4fb
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-02-23 21:29:55 +0100

    fix build scripts

--- build.sh
+++ build.sh
@@ -3,14 +3,4 @@
 mode=$1
 
 rm -rf _site
-echo "Building core..."
-./build-core.sh $mode
-echo "Building core done."
-
-echo "Building dancer..."
-./build-dancer.sh $mode
-echo "Building dancer done."
-
-echo "Building ranking..."
-./build-ranking.sh $mode
-echo "Building ranking done."
+node ./node_modules/@11ty/eleventy/cmd.js
